9ac1a4065071fb48f86a61b8f6f0bc1d0a0d373e
Arsemaabraha <arsema.abraha@chasacademy.se>
2025-04-29 16:28:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const wordCount = 0;
-
 if (wordCount === 0) {
-  console.log("Get writing!");
+    console.log("Get writing!"); 
 }
 
 const currentTemperature = 15;
 const goneForARun = false;
 
 if (currentTemperature > 10 && goneForARun === false) {
-  console.log("Go for a run!");
+    console.log("Go for a run!"); 
 }
 
-const name = "Arsema";
+const name = "Arsema"; 
 
 if (name.length > 10) {
-  console.log("You have a long name!");
+    console.log("You have a long name!");
 } else if (name.length < 5) {
-  console.log("You have a short name!");
+    console.log("You have a short name!");
 } else {
-  console.log("You have a medium length name!");
+    console.log("You have a medium length name!"); 
 }
